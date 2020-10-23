2b740ab2194571a7c7cb1a38f2623fdfecae8887
Dovbaka <dovbaka99@gmail.com>
2020-10-23 17:41:28 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+import styles from './App.module.css';
+import sample from './sample.json';
 
-function App() {
+import EmailEditor from 'react-email-editor';
+
+const App = () => {
+  const emailEditorRef = useRef(null);
+
+
+  const exportHtml = () => {
+    emailEditorRef.current.editor.exportHtml((data) => {
+      const { design, html } = data;
+      console.log('exportHtml', html);
+      console.log('exportJson', design);
+    });
+  };
+
+  const onLoad = () => {
+
+    emailEditorRef.current.editor.addEventListener('design:updated', function() { // Auto save to localStorage
+      emailEditorRef.current.editor.saveDesign(function(data) {
+        localStorage.setItem('Email-editor', JSON.stringify(data));
+      })
+    })
+
+    const localData = localStorage.getItem('Email-editor'); // Load design from localStorage
+    return localData ? emailEditorRef.current.editor.loadDesign(JSON.parse(localData)) :
+        emailEditorRef.current.editor.loadDesign(sample); // if localStorage is empty, load test design from sample.json
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <button onClick={exportHtml}>Export HTML</button>
+        </div>
+        <EmailEditor ref={emailEditorRef} onLoad={onLoad}/>
+      </div>
   );
-}
+};
 
 export default App;
