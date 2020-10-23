8887837ebf77cba23994a13e632f7c5967353dab
Dovbaka <dovbaka99@gmail.com>
2020-10-23 18:27:33 +0300

    Header adaptivity update

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
-import React, { useRef } from 'react';
+import React, {useRef} from 'react';
 import styles from './App.module.css';
 import sample from './sample.json';
 
 import EmailEditor from 'react-email-editor';
 
 const App = () => {
-  const emailEditorRef = useRef(null);
+    const emailEditorRef = useRef(null);
 
 
-  const exportHtml = () => {
-    emailEditorRef.current.editor.exportHtml((data) => {
-      const { design, html } = data;
-      console.log('exportHtml', html);
-      console.log('exportJson', design);
-    });
-  };
+    const exportHtml = () => {
+        emailEditorRef.current.editor.exportHtml((data) => {
+            const {design, html} = data;
+            console.log('exportHtml', html);
+            console.log('exportJson', design);
+        });
+    };
 
-  const onLoad = () => {
+    const onLoad = () => {
 
-    emailEditorRef.current.editor.addEventListener('design:updated', function() { // Auto save to localStorage
-      emailEditorRef.current.editor.saveDesign(function(data) {
-        localStorage.setItem('Email-editor', JSON.stringify(data));
-      })
-    })
+        emailEditorRef.current.editor.addEventListener('design:updated', function () { // Auto save to localStorage
+            emailEditorRef.current.editor.saveDesign(function (data) {
+                localStorage.setItem('Email-editor', JSON.stringify(data));
+            })
+        })
 
-    const localData = localStorage.getItem('Email-editor'); // Load design from localStorage
-    return localData ? emailEditorRef.current.editor.loadDesign(JSON.parse(localData)) :
-        emailEditorRef.current.editor.loadDesign(sample); // if localStorage is empty, load test design from sample.json
-  };
+        const localData = localStorage.getItem('Email-editor'); // Load design from localStorage
+        return localData ? emailEditorRef.current.editor.loadDesign(JSON.parse(localData)) :
+            emailEditorRef.current.editor.loadDesign(sample); // if localStorage is empty, load test design from sample.json
+    };
 
 
-  return (
-      <div className={styles.container}>
-        <div className={styles.header}>
-          <button onClick={exportHtml}>Export HTML</button>
+    return (
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <button onClick={exportHtml}>Export HTML</button>
+            </div>
+            <EmailEditor ref={emailEditorRef} onLoad={onLoad} style={{marginTop: "50px"}}/>
         </div>
-        <EmailEditor ref={emailEditorRef} onLoad={onLoad}/>
-      </div>
-  );
+    );
 };
 
 export default App;
